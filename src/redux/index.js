f3890a58f99b892f7cd7ed9b610abe30fe842425
iamnomandra <iamnomandra@gmail.com>
2025-05-11 20:55:41 +0530

    First upload

---
+++ src/redux/index.js
@@ -0,0 +1,15 @@
+import { combineReducers, legacy_createStore as createStore } from 'redux'
+import productsReducer from './productsReducer'
+import cartReducer from './cartReducer'
+import wishListReducer  from './wishListReducer'
+
+const reducer = combineReducers({
+  products: productsReducer,
+  cartItems: cartReducer,
+  wishList: wishListReducer,
+})
+
+export const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__?.()
+)
